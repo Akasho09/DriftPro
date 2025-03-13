1efeb64960e7b35db7f2176c6acecd9e3d3524b1
Akasho09 <aakashbwd@gmail.com>
2025-03-13 17:58:42 +0530

    namechngedddd

--- apps/user-app/components/RecentTrans.tsx
+++ apps/user-app/components/RecentTrans.tsx
@@ -15,7 +15,7 @@ export default async function ReTr() {
     return (
         <Card title="Add Money Transactions" className="p-4 border rounded-lg shadow-lg">
             <div className="space-y-4">
-                {data.map((d:any, index) => (
+                {data.map((d:any, index:any) => (
                     <div key={index} className="p-3 border-b rounded-lg bg-gray-50 shadow-sm">
                         <div className="text-lg font-semibold">Amount: ₹{(d.amount / 100).toFixed(2)}</div>
                         <div className="text-gray-700">Provider: {d.provider}</div>
